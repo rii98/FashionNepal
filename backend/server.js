9b208a1140b649fe3e880025a513b8c984d3ea0f
Roman Chhatkuli <romanchhatkuli09@gmail.com>
2025-02-23 18:24:00 +0545

    Added Some Features

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import paymentRouter from "./routes/paymentRoute.js";
 dotenv.config();
 
 
@@ -32,6 +33,7 @@ app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
+app.use('/api/payment',paymentRouter)
 
 app.get("/", (req, res) => {
   res.send("Working");
